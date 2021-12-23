6b2b7967fdad7d1644e7c03eb1d98020ac7847fa
TheFes <thefes+git@gmail.com>
2021-12-23 10:00:27 +0100

    🗑 Test with afvalwijzer integration

---
+++ custom_components/afvalwijzer/const/const.py
@@ -0,0 +1,53 @@
+from datetime import timedelta
+import logging
+
+API = "api"
+NAME = "afvalwijzer"
+VERSION = "2021.12.01"
+ISSUE_URL = "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
+
+SENSOR_PROVIDER_TO_URL = {
+    "afvalwijzer_data_default": [
+        "https://api.{0}.nl/webservices/appsinput/?apikey=5ef443e778f41c4f75c69459eea6e6ae0c2d92de729aa0fc61653815fbd6a8ca&method=postcodecheck&postcode={1}&street=&huisnummer={2}&toevoeging={3}&app_name=afvalwijzer&platform=phone&afvaldata={4}&langs=nl&"
+    ],
+}
+
+CONF_PROVIDER = "provider"
+CONF_API_TOKEN = "api_token"
+# 5ef443e778f41c4f75c69459eea6e6ae0c2d92de729aa0fc61653815fbd6a8ca
+CONF_POSTAL_CODE = "postal_code"
+CONF_STREET_NUMBER = "street_number"
+CONF_SUFFIX = "suffix"
+CONF_DATE_FORMAT = "date_format"
+CONF_INCLUDE_DATE_TODAY = "include_date_today"
+CONF_DEFAULT_LABEL = "default_label"
+CONF_ID = "id"
+CONF_EXCLUDE_LIST = "exclude_list"
+
+SENSOR_PREFIX = "afvalwijzer "
+SENSOR_ICON = "mdi:recycle"
+
+ATTR_LAST_UPDATE = "last_update"
+ATTR_IS_COLLECTION_DATE_TODAY = "is_collection_date_today"
+ATTR_IS_COLLECTION_DATE_TOMORROW = "is_collection_date_tomorrow"
+ATTR_IS_COLLECTION_DATE_DAY_AFTER_TOMORROW = "is_collection_date_day_after_tomorrow"
+ATTR_DAYS_UNTIL_COLLECTION_DATE = "days_until_collection_date"
+ATTR_YEAR_MONTH_DAY_DATE = "year_month_day_date"
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(hours=1)
+PARALLEL_UPDATES = 1
+SCAN_INTERVAL = timedelta(seconds=30)
+
+DOMAIN = "afvalwijzer"
+DOMAIN_DATA = "afvalwijzer_data"
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+Afvalwijzer
+This is a custom integration!
+If you have any issues with this you need to open an issue here:
+https://github.com/xirixiz/homeassistant-afvalwijzer/issues
+-------------------------------------------------------------------
+"""
---
+++ custom_components/afvalwijzer/sensor.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+"""
+Sensor component Afvalwijzer
+Author: Bram van Dartel - xirixiz
+"""
+
+from functools import partial
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+from requests.exceptions import HTTPError
+import voluptuous as vol
+
+from .const.const import (
+    _LOGGER,
+    CONF_DEFAULT_LABEL,
+    CONF_EXCLUDE_LIST,
+    CONF_ID,
+    CONF_INCLUDE_DATE_TODAY,
+    CONF_POSTAL_CODE,
+    CONF_PROVIDER,
+    CONF_STREET_NUMBER,
+    CONF_SUFFIX,
+    MIN_TIME_BETWEEN_UPDATES,
+    PARALLEL_UPDATES,
+    SCAN_INTERVAL,
+    STARTUP_MESSAGE,
+)
+from .provider.afvalwijzer import AfvalWijzer
+from .sensor_custom import AfvalwijzerCustomSensor
+from .sensor_provider import AfvalwijzerProviderSensor
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(
+            CONF_PROVIDER.strip().lower(), default="mijnafvalwijzer"
+        ): cv.string,
+        vol.Required(CONF_POSTAL_CODE.strip(), default="1234AB"): cv.string,
+        vol.Required(CONF_STREET_NUMBER.strip(), default="5"): cv.string,
+        vol.Optional(CONF_SUFFIX.strip(), default=""): cv.string,
+        vol.Optional(CONF_INCLUDE_DATE_TODAY.strip(), default="false"): cv.string,
+        vol.Optional(CONF_DEFAULT_LABEL.strip(), default="Geen"): cv.string,
+        vol.Optional(CONF_ID.strip().lower(), default=""): cv.string,
+        vol.Optional(CONF_EXCLUDE_LIST.strip().lower(), default=""): cv.string,
+    }
+)
+
+_LOGGER.info(STARTUP_MESSAGE)
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    provider = config.get(CONF_PROVIDER)
+    postal_code = config.get(CONF_POSTAL_CODE)
+    street_number = config.get(CONF_STREET_NUMBER)
+    suffix = config.get(CONF_SUFFIX)
+    include_date_today = config.get(CONF_INCLUDE_DATE_TODAY)
+    default_label = config.get(CONF_DEFAULT_LABEL)
+    exclude_list = config.get(CONF_EXCLUDE_LIST)
+
+    _LOGGER.debug("Afvalwijzer provider = %s", provider)
+    _LOGGER.debug("Afvalwijzer zipcode = %s", postal_code)
+    _LOGGER.debug("Afvalwijzer street_number = %s", street_number)
+
+    try:
+        afvalwijzer = await hass.async_add_executor_job(
+            partial(
+                AfvalWijzer,
+                provider,
+                postal_code,
+                street_number,
+                suffix,
+                include_date_today,
+                default_label,
+                exclude_list,
+            )
+        )
+    except ValueError as err:
+        _LOGGER.error("Check afvalwijzer platform settings %s", err.args)
+        raise
+
+    fetch_afvalwijzer_data = AfvalwijzerData(config)
+
+    waste_types_provider = afvalwijzer.waste_types_provider
+    _LOGGER.debug("Generating waste_types_provider list = %s", waste_types_provider)
+    waste_types_custom = afvalwijzer.waste_types_custom
+    _LOGGER.debug("Generating waste_types_custom list = %s", waste_types_custom)
+
+    entities = []
+
+    for waste_type in waste_types_provider:
+        _LOGGER.debug("Adding sensor provider: %s", waste_type)
+        entities.append(
+            AfvalwijzerProviderSensor(hass, waste_type, fetch_afvalwijzer_data, config)
+        )
+    for waste_type in waste_types_custom:
+        _LOGGER.debug("Adding sensor custom: %s", waste_type)
+        entities.append(
+            AfvalwijzerCustomSensor(hass, waste_type, fetch_afvalwijzer_data, config)
+        )
+
+    _LOGGER.debug("Entities appended = %s", entities)
+    async_add_entities(entities)
+
+
+class AfvalwijzerData(object):
+    def __init__(self, config):
+        self.config = config
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        provider = self.config.get(CONF_PROVIDER)
+        postal_code = self.config.get(CONF_POSTAL_CODE)
+        street_number = self.config.get(CONF_STREET_NUMBER)
+        suffix = self.config.get(CONF_SUFFIX)
+        include_date_today = self.config.get(CONF_INCLUDE_DATE_TODAY)
+        default_label = self.config.get(CONF_DEFAULT_LABEL)
+        exclude_list = self.config.get(CONF_EXCLUDE_LIST)
+
+        try:
+            afvalwijzer = AfvalWijzer(
+                provider,
+                postal_code,
+                street_number,
+                suffix,
+                include_date_today,
+                default_label,
+                exclude_list,
+            )
+        except ValueError as err:
+            _LOGGER.error("Check afvalwijzer platform settings %s", err.args)
+            raise
+
+        # waste data provider update - with today
+        try:
+            self.waste_data_with_today = afvalwijzer.waste_data_with_today
+        except ValueError as err:
+            _LOGGER.error("Check waste_data_provider %s", err.args)
+            self.waste_data_with_today = default_label
+            raise
+
+        # waste data provider update - without today
+        try:
+            self.waste_data_without_today = afvalwijzer.waste_data_without_today
+        except ValueError as err:
+            _LOGGER.error("Check waste_data_provider %s", err.args)
+            self.waste_data_without_today = default_label
+            raise
+
+        # waste data custom update
+        try:
+            self.waste_data_custom = afvalwijzer.waste_data_custom
+        except ValueError as err:
+            _LOGGER.error("Check waste_data_custom %s", err.args)
+            self.waste_data_custom = default_label
+            raise
