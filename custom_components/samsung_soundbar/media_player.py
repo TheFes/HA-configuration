1a7ac617c2b64c448ae613ee91eb378ac97568dd
TheFes <thefes+git@gmail.com>
2022-01-25 09:18:53 +0100

    🔨 Replace soundbar custom component with own fork

--- custom_components/samsung_soundbar/media_player.py
+++ custom_components/samsung_soundbar/media_player.py
@@ -86,7 +86,7 @@ class MultiRoomApi():
     url = '{0}/{1}?{2}'.format(self.endpoint, mode, query)
 
     try:
-      with async_timeout.timeout(TIMEOUT, loop=self.hass.loop):
+      with async_timeout.timeout(TIMEOUT):
         _LOGGER.debug("Executing: {} with cmd: {}".format(url, cmd))
         response = await self.session.get(url)
         data = await response.text()
@@ -291,18 +291,24 @@ class MultiRoomDevice(MediaPlayerEntity):
         self._state = STATE_ON
         "Get Current Source"
         source = await self.api.get_source()
-        "Source 0 is type on input"
-        if source[0]:
-          self._current_source = source[0]
-        "Source 1 is input mode"
-        if source[1]:
-          self._mode = source[1]
+        if source is not None:
+          "Source 0 is type on input"
+          if source[0]:
+            self._current_source = source[0]
+          "Source 1 is input mode"
+          if source[1]:
+            self._mode = source[1]
+          else:
+            self._mode = ''
         else:
-          self._mode = ''
-        "Get Volume"
-        volume = await self.api.get_volume()
-        if volume[0]:
-          self._volume = int(volume[0]) / self._max_volume
+            self._mode = ''
+        try:
+          "Get Volume"
+          volume = await self.api.get_volume()
+          if volume[0]:
+            self._volume = int(volume[0]) / self._max_volume
+        except:
+          _LOGGER.error("Failed to get volume")
         "Get Mute State"
         muted = await self.api.get_muted()
         if muted:
