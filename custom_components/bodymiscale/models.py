f3e1fa49c48e267fc6f0ce32cd8de8340984869f
TheFes <thefes+git@gmail.com>
2022-05-09 10:49:49 +0200

    ❌ No longer add HACS to Github

--- custom_components/bodymiscale/models.py
+++
@@ -1,9 +0,0 @@
-"""Models module."""
-from enum import Enum
-
-
-class Gender(str, Enum):
-    """Gender enum."""
-
-    MALE = "male"
-    FEMALE = "female"
--- custom_components/neerslag/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants for the Neerslag Sensor (Buienalarm / Buienradar) integration."""
-
-DOMAIN = "neerslag"
-FRONTEND_SCRIPT_URL = "/neerslag-card.js"
-DATA_EXTRA_MODULE_URL = 'frontend_extra_module_url'
